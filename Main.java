4a36cfa6f5531357516c9a512dc4624448493b32
elsehj <85619911+elsehj@users.noreply.github.com>
2021-10-11 17:39:54 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-	public static void main(String[] args) {
-		KaartOpMaat komObject = new KaartOpMaat();
-		komObject.makeKaartOpMaat();
-
-		System.out.println(komObject.kaartBeschrijving());
-	}
-}
